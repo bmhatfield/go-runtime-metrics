65ec3e15d60f1cc4dd84d33ac2dd20e0b39cce07
Brian Hatfield <bmhatfield@gmail.com>
2014-09-02 17:33:20 -0400

    Loop collector

--- runstats.go
+++ runstats.go
@@ -38,54 +38,56 @@ func init() {
 }
 
 func collector() {
-	if *CPU {
-		// Goroutines
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "cpu.goroutines"), strconv.Itoa(runtime.NumGoroutine()))
+	for {
+		if *CPU {
+			// Goroutines
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "cpu.goroutines"), strconv.Itoa(runtime.NumGoroutine()))
 
-		// CGo calls
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "cpu.cgo_calls"), strconv.FormatUint(uint64(runtime.NumCgoCall()), 10))
-	}
-
-	if *MEM {
-		m := new(runtime.MemStats)
-		runtime.ReadMemStats(m)
-
-		// General
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.alloc"), strconv.FormatUint(m.Alloc, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.total"), strconv.FormatUint(m.TotalAlloc, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.sys"), strconv.FormatUint(m.Sys, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.lookups"), strconv.FormatUint(m.Lookups, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.malloc"), strconv.FormatUint(m.Mallocs, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.frees"), strconv.FormatUint(m.Frees, 10))
-
-		// Heap
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.alloc"), strconv.FormatUint(m.HeapAlloc, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.sys"), strconv.FormatUint(m.HeapSys, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.idle"), strconv.FormatUint(m.HeapIdle, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.inuse"), strconv.FormatUint(m.HeapInuse, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.released"), strconv.FormatUint(m.HeapReleased, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.objects"), strconv.FormatUint(m.HeapObjects, 10))
-
-		// Stack
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.inuse"), strconv.FormatUint(m.StackInuse, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.sys"), strconv.FormatUint(m.StackSys, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mspan_inuse"), strconv.FormatUint(m.MSpanInuse, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mspan_sys"), strconv.FormatUint(m.MSpanSys, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mcache_inuse"), strconv.FormatUint(m.MCacheInuse, 10))
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mcache_sys"), strconv.FormatUint(m.MCacheSys, 10))
-
-		s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.othersys"), strconv.FormatUint(m.OtherSys, 10))
-
-		if *GC {
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.sys"), strconv.FormatUint(m.GCSys, 10))
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.next"), strconv.FormatUint(m.NextGC, 10))
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.last"), strconv.FormatUint(m.LastGC, 10))
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.pause_total"), strconv.FormatUint(m.PauseTotalNs, 10))
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.pause"), strconv.FormatUint(m.PauseNs[(m.NumGC+255)%256], 10))
-			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.count"), strconv.FormatUint(uint64(m.NumGC), 10))
+			// CGo calls
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "cpu.cgo_calls"), strconv.FormatUint(uint64(runtime.NumCgoCall()), 10))
 		}
-	}
 
-	// Don't collect for a time interval
-	time.Sleep(time.Duration(*pause) * time.Second)
+		if *MEM {
+			m := new(runtime.MemStats)
+			runtime.ReadMemStats(m)
+
+			// General
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.alloc"), strconv.FormatUint(m.Alloc, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.total"), strconv.FormatUint(m.TotalAlloc, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.sys"), strconv.FormatUint(m.Sys, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.lookups"), strconv.FormatUint(m.Lookups, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.malloc"), strconv.FormatUint(m.Mallocs, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.frees"), strconv.FormatUint(m.Frees, 10))
+
+			// Heap
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.alloc"), strconv.FormatUint(m.HeapAlloc, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.sys"), strconv.FormatUint(m.HeapSys, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.idle"), strconv.FormatUint(m.HeapIdle, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.inuse"), strconv.FormatUint(m.HeapInuse, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.released"), strconv.FormatUint(m.HeapReleased, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.heap.objects"), strconv.FormatUint(m.HeapObjects, 10))
+
+			// Stack
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.inuse"), strconv.FormatUint(m.StackInuse, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.sys"), strconv.FormatUint(m.StackSys, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mspan_inuse"), strconv.FormatUint(m.MSpanInuse, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mspan_sys"), strconv.FormatUint(m.MSpanSys, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mcache_inuse"), strconv.FormatUint(m.MCacheInuse, 10))
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.stack.mcache_sys"), strconv.FormatUint(m.MCacheSys, 10))
+
+			s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.othersys"), strconv.FormatUint(m.OtherSys, 10))
+
+			if *GC {
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.sys"), strconv.FormatUint(m.GCSys, 10))
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.next"), strconv.FormatUint(m.NextGC, 10))
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.last"), strconv.FormatUint(m.LastGC, 10))
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.pause_total"), strconv.FormatUint(m.PauseTotalNs, 10))
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.pause"), strconv.FormatUint(m.PauseNs[(m.NumGC+255)%256], 10))
+				s.Gauge(1.0, fmt.Sprintf("%s.%s", prefix, "mem.gc.count"), strconv.FormatUint(uint64(m.NumGC), 10))
+			}
+		}
+
+		// Don't collect for a time interval
+		time.Sleep(time.Duration(*pause) * time.Second)
+	}
 }
